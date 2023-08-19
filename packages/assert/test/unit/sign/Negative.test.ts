174877b716d8618a98a2fe3f49a5241b8ed5e316
Jaron <jaron_zijlstra@outlook.com>
2023-08-19 18:11:20 +0200

    test: merge sign tests where possible

--- packages/assert/test/unit/sign/Negative.test.ts
+++ packages/assert/test/unit/sign/Negative.test.ts
@@ -28,52 +28,39 @@ describe("Negative tests", () => {
 			expect(() => new Test()).not.toThrow();
 		});
 
-		// TODO: merge tests under this comment
-		test.each([0, 1, 32, 85])("GIVEN %s THEN throws", (value) => {
-			class Test {
-				@Assert.Negative
-				public number = value;
+		test.each([0, 1, 32, 85, 0n, 1n, 32n, 85n])(
+			"GIVEN %s THEN throws",
+			(value) => {
+				class Test {
+					@Assert.Negative
+					public number = value;
+				}
+
+				expect(() => new Test()).toThrow(
+					new CombinedError([
+						new ExpectedConstraintError(
+							`s.${typeof value as "number" | "bigint"}.lessThan`,
+							`Invalid ${
+								typeof value as "number" | "bigint"
+							} value`,
+							value,
+							`expected < 0${
+								typeof value === "bigint" ? "n" : ""
+							}`
+						),
+						new ValidationError(
+							`s.${
+								typeof value === "number" ? "bigint" : "number"
+							}`,
+							`Expected a ${
+								typeof value === "number" ? "bigint" : "number"
+							} primitive`,
+							value
+						)
+					])
+				);
 			}
-
-			expect(() => new Test()).toThrow(
-				new CombinedError([
-					new ExpectedConstraintError(
-						"s.number.lessThan",
-						"Invalid number value",
-						value,
-						"expected < 0"
-					),
-					new ValidationError(
-						"s.bigint",
-						"Expected a bigint primitive",
-						value
-					)
-				])
-			);
-		});
-
-		test.each([0n, 1n, 32n, 85n])("GIVEN %s THEN throws", (value) => {
-			class Test {
-				@Assert.Negative
-				public bigint = value;
-			}
-
-			expect(() => new Test()).toThrow(
-				new CombinedError([
-					new ExpectedConstraintError(
-						"s.bigint.lessThan",
-						"Invalid bigint value",
-						value,
-						"expected < 0"
-					),
-					new ValidationError(
-						"s.number",
-						"Expected a number primitive",
-						value
-					)
-				])
-			);
-		});
+		);
 	});
 
 	describe("Parameter decorator", () => {
--- packages/assert/test/unit/sign/Positive.test.ts
+++ packages/assert/test/unit/sign/Positive.test.ts
@@ -28,52 +28,41 @@ describe("Positive tests", () => {
 			expect(() => new Test()).not.toThrow();
 		});
 
-		// TODO: merge tests under this comment
-		test.each([-1, -32, -85])("GIVEN %s THEN throws", (value) => {
-			class Test {
-				@Assert.Positive
-				public number = value;
+		test.each([-1, -32, -85, -1n, -32n, -85n])(
+			"GIVEN %s THEN throws",
+			(value) => {
+				class Test {
+					@Assert.Positive
+					public number = value;
+				}
+
+				expect(() => new Test()).toThrow(
+					new CombinedError([
+						new ExpectedConstraintError(
+							`s.${
+								typeof value as "number" | "bigint"
+							}.greaterThanOrEqual`,
+							`Invalid ${
+								typeof value as "number" | "bigint"
+							} value`,
+							value,
+							`expected >= 0${
+								typeof value === "number" ? "" : "n"
+							}`
+						),
+						new ValidationError(
+							`s.${
+								typeof value === "number" ? "bigint" : "number"
+							}`,
+							`Expected a ${
+								typeof value === "number" ? "bigint" : "number"
+							} primitive`,
+							value
+						)
+					])
+				);
 			}
-
-			expect(() => new Test()).toThrow(
-				new CombinedError([
-					new ExpectedConstraintError(
-						"s.number.greaterThanOrEqual",
-						"Invalid number value",
-						value,
-						"expected >= 0"
-					),
-					new ValidationError(
-						"s.bigint",
-						"Expected a bigint primitive",
-						value
-					)
-				])
-			);
-		});
-
-		test.each([-1n, -32n, -85n])("GIVEN %s THEN throws", (value) => {
-			class Test {
-				@Assert.Positive
-				public bigint = value;
-			}
-
-			expect(() => new Test()).toThrow(
-				new CombinedError([
-					new ExpectedConstraintError(
-						"s.bigint.greaterThanOrEqual",
-						"Invalid bigint value",
-						value,
-						"expected >= 0"
-					),
-					new ValidationError(
-						"s.number",
-						"Expected a number primitive",
-						value
-					)
-				])
-			);
-		});
+		);
 	});
 
 	describe("Parameter decorator", () => {
